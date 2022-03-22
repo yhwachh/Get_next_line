260816f4693b88b872b61fde756234edc27e0ea8
Ibraguim Albakov <ibraguimalbakov@MacBook-Air-de-Ibraguim.local>
2022-03-23 00:30:48 +0100

    2 commit

--- get_next_line.h
+++ get_next_line.h
@@ -10,18 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <stdlib.h>
+# define BUFFER_SIZE 100
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
-# include <string.h>
-
-char *get_next_line(int fd);
-
+# include <stdlib.h>
+# include <fcntl.h>
 
 
+char    *get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
 
 
 
--- get_next_line_utiles.c
+++ get_next_line_utiles.c
@@ -10,4 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*f;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	f = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!f)
+		return (NULL);
+	while (s1[i])
+	{
+		f[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		f[i] = s2[j];
+		i++;
+		j++;
+	}
+	f[i] = '\0';
+	return (f);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
