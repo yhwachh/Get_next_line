e7184873cc64371e3b27b9f1624a9bf48c2db5ec
Ibraguim Albakov <ibraguimalbakov@MacBook-Air-de-Ibraguim.local>
2022-05-17 09:47:24 +0200

    home

--- get_next_line.c
+++ get_next_line.c
@@ -12,118 +12,99 @@
 
 #include "get_next_line.h"
 
-// join and free
-char	*ft_free(char *buffer, char *buf)
+char	*ft_join_and_free(char *save, char *buf)
 {
 	char	*temp;
 
-	temp = ft_strjoin(buffer, buf);
-	free(buffer);
+	temp = ft_strjoin(save, buf);
+	free(save);
 	return (temp);
 }
 
-// delete line find
-char	*ft_next(char *buffer)
+char	*ft_next_line(char *buffer)
 {
 	int		i;
 	int		j;
 	char	*line;
 
 	i = 0;
-	// find len of first line
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	// if eol == \0 return NULL
 	if (!buffer[i])
 	{
 		free(buffer);
 		return (NULL);
 	}
-	// len of file - len of firstline + 1
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
 	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
-	// line == buffer
 	while (buffer[i])
 		line[j++] = buffer[i++];
 	free(buffer);
 	return (line);
 }
 
-// take line for return
 char	*ft_line(char *buffer)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
-	// if no line return NULL
 	if (!buffer[i])
 		return (NULL);
-	// go to the eol
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	// malloc to eol
-	line = ft_calloc(i + 2, sizeof(char));
+	i = i + 2;
+	line = ft_calloc(i , sizeof(char));
 	i = 0;
-	// line = buffer
 	while (buffer[i] && buffer[i] != '\n')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
-	// if eol is \0 or \n, replace eol by \n
 	if (buffer[i] && buffer[i] == '\n')
 		line[i++] = '\n';
 	return (line);
 }
-
-char	*read_file(int fd, char *res)
-{
-	char	*buffer;
-	int		byte_read;
-
-	// malloc if res dont exist
-	if (!res)
-		res = ft_calloc(1, 1);
-	// malloc buffer
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	byte_read = 1;
-	while (byte_read > 0)
-	{
-		// while not eof read
-		byte_read = read(fd, buffer, BUFFER_SIZE);
-		if (byte_read == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		// 0 to end for leak
-		buffer[byte_read] = 0;
-		// join and free
-		res = ft_free(res, buffer);
-		// quit if \n find
-		if (ft_strchr(buffer, '\n'))
-			break ;
-	}
-	free(buffer);
-	return (res);
-}
-
+	
 char	*get_next_line(int fd)
 {
 	static char	*save;
 	char		*line;
+	char		*buf;
+	int			len;
 
-	// error handling
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (NULL);
-	save = read_file(fd, save);
+	if (!save)
+		save = ft_calloc(1, 1);
+	buf = ft_test(fd, save);
+	len = 1;
+	while (len > 0)
+	{
+		len = read(fd, buf, BUFFER_SIZE);
+		if (len == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[len] = 0;
+		save = ft_join_and_free(save, buf);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	free(buf);
 	if (!save)
 		return (NULL);
 	line = ft_line(save);
-	save = ft_next(save);
+	save = ft_next_line(save);
 	return (line);
+}
+
+char	*ft_test(int fd, char *buf)
+{
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	return (buf);
 }
 
 
@@ -133,6 +114,12 @@ int	main(void)
 	char	*line;
 
 	fd = open("hello", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
 	line = get_next_line(fd);
 	printf("%s", line);
 	line = get_next_line(fd);
--- get_next_line.h
+++ get_next_line.h
@@ -25,6 +25,7 @@
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *string, int searchedChar );
+char	*ft_test(int fd, char *buf);
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
